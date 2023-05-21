f67a4066a892c34cc74b4ad9809d3604f147f6ed
DanhDue ExOICTIF <danhdue@gmail.com>
2023-05-21 19:22:32 +0700

    update documents.

--- scripts/buildAPKRelease.sh
+++ scripts/buildAPKRelease.sh
@@ -7,7 +7,7 @@ fluttergen -c pubspec.yaml
 get generate locales assets/locales
 git ls-files -z -- '*.dart' | xargs -0 dart format "$@" -l 99
 
-fvm flutter build appbundle --obfuscate --split-debug-info --split-per-abi \
+fvm flutter build apk --obfuscate --split-debug-info --split-per-abi \
 --dart-define=DART_DEFINES_APP_NAME="GetXSample" \
 --dart-define=DART_DEFINES_BASE_URL="https://danhdue.com/api/v1" \
 --release \
